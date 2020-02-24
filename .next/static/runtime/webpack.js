bbaf063a2a6da2bb61f5ef05a9afa1ee5b919408
Thang Luong <thangluong@kms.com.vn>
2020-02-24 09:17:13 +0700

    update git ignore to .next folder

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1e9b464b2a090fa4227a";
+/******/ 	var hotCurrentHash = "2aaadf5f72c4c89f3a45";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
